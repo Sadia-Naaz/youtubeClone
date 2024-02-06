657e6537e83f3adc87c12d14efc4e84101f46857
Sadia-Naaz <sadia244901@gmail.com>
2024-02-06 17:10:20 +0530

    srecond commit

--- youtube.js
+++ youtube.js
@@ -1,18 +1,22 @@
 const API_KEY = "AIzaSyASLrY_Iy991zuLtrQP066_fNSsBD-Lqdo";
 const BASE_URL = "https://www.googleapis.com/youtube/v3";
 
+let searchButton = document.querySelector(".search-btn");
+let searchInput = document.querySelector(".search-bar");
+
 window.addEventListener("DOMContentLoaded", () => {
     fetchVideos("Learn JS", 10);
-});
 
+});
 async function fetchVideos(searchQuery, maxResults) {
     const response = await fetch(
         `${BASE_URL}/search?key=${API_KEY}&q=${searchQuery}&maxResults=${maxResults}&part=snippet`
     );
     const data = await response.json();
-    console.log(data);
     displayVideos(data.items);
 }
+
+
 
 function displayVideos(videos) {
     const container = document.getElementById("video-gallery");
@@ -33,38 +37,33 @@ function displayVideos(videos) {
         `;
         container.appendChild(videoCard);
     });
-// }
-// searchButton.addEventListener("click", () => {
-//     const searchValue = searchInput.value.trim(); // Trim whitespace from the search query
-//     if (searchValue) {
-//         fetchVideos(searchValue);
-//     } else {
-//       alert("Please enter a valid search query");
-//     }
-//   });
-//   async function fetchSearchResults(searchString) {
-//     // searchString will the input entered by the user
-//     const endpoint = `${baseUrl}/search?key=${API_KEY}&q=${searchString}&part=snippet&maxResults=50`;
-//     try {
-//       const response = await fetch(endpoint);
-//       const result = await response.json();
+}
+searchButton.addEventListener("click", () => {
+    const searchValue = searchInput.value.trim(); // Trim whitespace from the search query
+    if (searchValue) {
+        fetchSearchResults(searchValue);
+    } else {
+      alert("Please enter a valid search query");
+    }
+  });
+  async function fetchSearchResults(searchString) {
+    // searchString will the input entered by the user
+    const endpoint = `${BASE_URL}/search?key=${API_KEY}&q=${searchString}&part=snippet&maxResults=50&part=snippet`;
+   
+    try {
+      const response = await fetch(endpoint);
+      const result = await response.json();
   
-//       for (let i = 0; i < result.items.length; i++) {
+      for (let i = 0; i < result.items.length; i++) {
   
-//         let videoId = result.items[i].id.videoId;
-//         let channelId = result.items[i].snippet.channelId;
-//         console.log(videoId);
-//         console.log(channelId);
-  
-//         let statistics = await getVideoStatistics(videoId);
-//         let channelLogo = await fetchChannelLogo(channelId);
-  
-//         result.items[i].statistics = statistics;
-//         result.items[i].channelLogo = channelLogo;
-//       }
-  
-//       renderVideosOntoUI(result.items); // 2
-//     } catch (error) {
-//       alert("Some error occured");
-//     }
+        let videoId = result.items[i].id.videoId;
+        let channelId = result.items[i].snippet.channelId;
+        console.log(videoId);
+        console.log(channelId);
+      }
+      displayVideos(result.items)
+    //   renderVideosOntoUI(result.items); // 2
+    } catch (error) {
+      alert("Some error occured");
+    }
   }
